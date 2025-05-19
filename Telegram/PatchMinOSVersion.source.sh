b03178483e8314e765eefc7d9865cce1e81e390f
Kylmakalle <iceman9831@gmail.com>
2025-05-19 23:42:53 +0300

    Fix iOS 13-14 concurrency backport

--- Telegram/PatchMinOSVersion.source.sh
+++ Telegram/PatchMinOSVersion.source.sh
@@ -13,3 +13,11 @@ if [ "$version" == "14.0" ]; then
 	binary_path="$f/$(basename $f | sed -e s/\.appex//g)"
 	xcrun lipo "$binary_path" -remove armv7 -o "$binary_path" 2>/dev/null || true
 fi
+
+# MARK: Swiftgram
+find "$1" -type f \( -perm +111 -o -name "*.dylib" \) | while read -r bin; do
+  if otool -L "$bin" | grep -q "/usr/lib/swift/libswift_Concurrency.dylib"; then
+    echo "Patching concurrency backport in: $bin"
+    install_name_tool -change /usr/lib/swift/libswift_Concurrency.dylib @rpath/libswift_Concurrency.dylib "$bin"
+  fi
+done
